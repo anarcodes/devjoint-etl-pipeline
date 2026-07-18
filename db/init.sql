8bbfe73973bd59b07dfcd7922bbafe49d608b37c
Anar Valizada <anar.velizade.2003@mail.ru>
2026-07-18 22:07:42 +0400

    clean up unused init.sql file

--- db/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS final_users_report (
-    id INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    city VARCHAR(100),
-    company VARCHAR(100),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
